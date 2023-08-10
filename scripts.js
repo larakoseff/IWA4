b2361fb588e736dbf24cf41db438514724b5d2ac
larakoseff <larakoseff@gmail.com>
2023-08-10 11:02:59 +0200

    Add month variable so no need to redeclare year

--- scripts.js
+++ scripts.js
@@ -1,3 +1,39 @@
+// const date: 2050
+// const status: 'student'
+// const count = 0
+
+// if (date = 2050) {
+// 	console.log("January", 'New Year’s Day'
+// 	console.log("March", 'Human Rights Day')
+// 	date = 'April'
+// 	console.log(date, 'Family Day')
+// 	console.log(date, 'Freedom Day')
+// 	let count = count + 4
+
+// 	if (status = "student") {
+// 	  console.log('June', 'Youth Day')
+// 		let count = count + 1
+//   }
+
+// 	console.log('August', 'Women’s Day')
+// 	console.log('September', 'Heritage Day')
+// 	date = 'December'
+// 	console.log(date, 'Day of Reconciliation')
+// 	let count = count + 3
+
+// 	if (status = "parent") {
+// 	  console.log(date, 'Christmas Day')
+// 		let count = count + 1
+//   }
+
+// 	console.log(date, 'Day of Goodwill')
+// 	let count = count + 1
+// }
+
+// console.log('Your status is:', status)
+// console.log('The year is:', date)
+// console.log('The total holidays is:', count)
+
 let date = 2050;
 let status1 = "student";
 let count = 0;
@@ -5,31 +41,30 @@ let count = 0;
 if (date = 2050) {
   console.log("January", "New Year's Day");
   console.log("March", "Human Rights Day");
-  date = "April";
-  console.log(date, "Family Day");
-  console.log(date, "Freedom Day");
+  month = "April";
+  console.log(month, "Family Day");
+  console.log(month, "Freedom Day");
   count = count + 4;
 
-  if (status1 == "student") {
+  if (status1 === "student") {
     console.log("June", "Youth Day");
     count = count + 1;
   }
 
   console.log("August", "Women's Day");
   console.log("September", "Heritage Day");
-  date = "December";
-  console.log(date, "Day of Reconciliation");
+  month = "December";
+  console.log(month, "Day of Reconciliation");
   count = count + 3;
 
-  if (status1 == "parent") {
-    console.log(date, "Christmas Day");
+  if (status1 === "parent") {
+    console.log(month, "Christmas Day");
     count = count + 1;
   }
 
-  console.log(date, "Day of Goodwill");
+  console.log(month, "Day of Goodwill");
   count = count + 1;
 
-  date = 2050;
 }
 
 console.log("Your status is:", status1);
